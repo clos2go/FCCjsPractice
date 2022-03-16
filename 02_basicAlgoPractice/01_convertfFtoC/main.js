e2368cadb7af4632d460f32661986002cf5549e2
lopezlbj <carlos.lopez@austinisd.org>
2022-03-16 15:29:17 -0500

    reverse str prob

--- 02_basicAlgoPractice/01_convertfFtoC/main.js
+++ 02_basicAlgoPractice/01_convertfFtoC/main.js
@@ -8,3 +8,20 @@ console.clear()
 // Use the variable fahrenheit already defined and assign it the
 // Fahrenheit temperature equivalent to the given Celsius temperature.
 // Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.
+
+function convertToF(celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32
+  return fahrenheit
+}
+
+console.log(convertToF(0)) //should return a number
+
+console.log(convertToF(-30)) //should return a value of -22
+
+console.log(convertToF(-10)) //should return a value of 14
+
+console.log(convertToF(0)) //should return a value of 32
+
+console.log(convertToF(20)) //should return a value of 68
+
+console.log(convertToF(30)) //should return a value of 86
