1656226e321c6bcb5a417c4d5aa55e9962e427e9
lopezlbj <carlos.lopez@austinisd.org>
2022-03-18 10:29:19 -0500

    2 practice porbs: mutations and chunkyMonkey

--- 02_basicAlgoPractice/15_mutations/main.js
+++ 02_basicAlgoPractice/15_mutations/main.js
@@ -18,8 +18,8 @@ function mutation(arr) {
   let target = arr[0].toLowerCase()
   for (let i = 0; i < test.length; i++) {
     if (target.indexOf(test[i]) < 0) return false
+    return true
   }
-  return true
 }
 
 console.log(mutation(['hello', 'hey'])) //should return false.
--- 02_basicAlgoPractice/16_chunkyMonkey/main.js
+++ 02_basicAlgoPractice/16_chunkyMonkey/main.js
@@ -24,10 +24,60 @@ function chunkArrayInGroups(arr, size) {
   return newArr
 }
 
+// //2 empty arrays > for loops test > push to temp arr in loop > modulus > temp = empty => push result
+// function chunkArrayInGroups(arr, size) {
+//   let temp = []
+//   let result = []
+
+//   for (let a = 0; a < arr.length; a++) {
+//     if (a % size !== size - 1) temp.push(arr[a])
+//     else {
+//       temp.push(arr[a])
+//       result.push(temp)
+//       temp = []
+//     }
+//   }
+//   if (temp.length !== 0) result.push(temp)
+//   return result
+// }
+
+// //empty arr > loop until arr.length > in loop, push using slice(i + i + size) > increment size onto i
+// function chunkArrayInGroups(arr, size) {
+//   let newArr = []
+//   let i = 0
+
+//   while (i < arr.length) {
+//     newArr.push(arr.slice(i, i + size))
+//     i += size
+//   }
+//   return newArr
+// }
+
+// //cleaner than up one
+// function chunkArrayInGroups(arr, size) {
+//   let newArr = []
+//   while (arr.length > 0) {
+//     newArr.push(arr.splice(0, size))
+//   }
+//   return newArr
+// }
+
+// //Recursive
+
+// function chunkArrayInGroups(arr, size) {
+//   if (arr.length <= size) {
+//     return [arr]
+//   } else {
+//     return [arr.slice(0, size)].concat(
+//       chunkArrayInGroups(arr.slice(size), size)
+//     )
+//   }
+// }
+
 console.log(chunkArrayInGroups(['a', 'b', 'c', 'd'], 2)) //should return [["a", "b"], ["c", "d"]].
-// console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3)) //should return [[0, 1, 2], [3, 4, 5]].
-// console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2)) //should return [[0, 1], [2, 3], [4, 5]].
-// console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4)) //should return [[0, 1, 2, 3], [4, 5]].
-// console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)) //should return [[0, 1, 2], [3, 4, 5], [6]].
-// console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)) //should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
-// console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)) //should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]]
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3)) //should return [[0, 1, 2], [3, 4, 5]].
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2)) //should return [[0, 1], [2, 3], [4, 5]].
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4)) //should return [[0, 1, 2, 3], [4, 5]].
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)) //should return [[0, 1, 2], [3, 4, 5], [6]].
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)) //should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)) //should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]]
